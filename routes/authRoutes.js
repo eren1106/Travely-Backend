c7c5e1225ec2474068e5d8675e1daf68328de327
TAN JIAU YAN <yanyantan0816@gmail.com>
2023-05-22 22:42:22 +0800

    authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,8 @@
 import express from 'express';
 
 const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 // Reusable middleware function for handling errors
 const handleErrors = (res, err) => {
@@ -11,7 +13,22 @@ const handleErrors = (res, err) => {
 // REGISTER
 router.post('/register', async (req, res) => {
   try {
-    
+
+    //generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    //save user and respond
+    const user = await newUser.save();
+    res.status(200).json(user);
+
   }
   catch (err) {
     handleErrors(res, err);
@@ -21,6 +38,16 @@ router.post('/register', async (req, res) => {
 // LOGIN
 router.post('/login', async (req, res) => {
   try {
+
+    // find user with unique email
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+
+    // validate password by comparing with the actual password
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    !validPassword && res.status(400).json("wrong password")
+
+    res.status(200).json(user)
 
   }
   catch (err) {
