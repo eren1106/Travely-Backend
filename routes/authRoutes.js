dd014d9a5de403103983183146b24dda9f4a7d95
Ryanangry07 <S2022048@siswa.um.edu.my>
2023-06-13 07:32:47 +0800

    add on forget password function
    first version, just update the password without verification, still need email verification, will update later

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -25,6 +25,7 @@ router.post('/register', async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
+    console.log(req.body)
     //create new user
     const newUser = new User({
       username: req.body.username,
@@ -54,6 +55,30 @@ router.post('/login', async (req, res) => {
     const validPassword = await bcrypt.compare(req.body.password, user.password)
     !validPassword && res.status(400).json("wrong password")
 
+    res.status(200).json(user)
+
+  }
+  catch (err) {
+    handleErrors(res, err);
+  }
+});
+
+// RESET PASSWORD
+router.post('/resetPassword', async (req, res) => {
+  try {
+
+    // find user with unique email
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+
+    // validate by sending email
+
+    //generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.newPassword, salt);
+    user.password = hashedPassword
+    //save user and respond
+    const newUser = await user.save();
     res.status(200).json(user)
 
   }
