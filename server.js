304d1897807b6bef6e7200036a8e767fd6b24b09
WuBang <wbwbwbzheng@gmail.com>
2023-06-13 12:13:03 +0800

    Fetech post

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // GUIDE TO USE THIS APP
 // run 'npm install' at terminal to install all packages (first time only)
 // run 'npm run dev' at terminal to start the app
-
 import express from 'express';
 import mongoose from 'mongoose';
 import authRoutes from './routes/authRoutes.js';
@@ -34,24 +33,34 @@ const connectDB = async () => {
 connectDB();
 
 
+// let frontend read images
 const currentWorkingDirectory = process.cwd();
 app.use("/images", express.static(path.join(currentWorkingDirectory, "public/images")));
+
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "public/images");
   },
   filename: (req, file, cb) => {
-    //cb(null,  file.originalname);
+
     const fileName = req.body.name; // Use the filename sent from the frontend
     cb(null, fileName);
   },
 });
 
 const upload = multer({storage});
+// app.post("/api/upload", upload.array('file',10),(req, res) => {
+//   try {
+//     console.log(req.files); // Logs any files
+//     return res.status(200).json("File uploded successfully");
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
+
 app.post("/api/upload", upload.single("file"), (req, res) => {
   try {
-    console.log(req.body); // Logs form body values
-    console.log(req.files); // Logs any files
     return res.status(200).json("File uploded successfully");
   } catch (error) {
     console.error(error);
